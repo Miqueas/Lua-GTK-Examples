0482be3bd282f8c311b5aecc51bd9e1ab4db9f9e
Miqueas Martinez <miqueas2020@yahoo.com>
2021-01-17 23:55:38 -0500

    Added GtkLabel example 2

---
+++ GtkLabel2.lua
@@ -0,0 +1,38 @@
+local lgi = require("lgi")
+local Gtk = lgi.require("Gtk", "3.0")
+
+local App = Gtk.Application({
+  application_id = "com.github.moonsteal.lua-gtk-examples.GtkLabel"
+})
+
+function App:on_startup()
+  Gtk.ApplicationWindow({
+    application = self,
+    default_width = 400,
+    default_height = 400
+  })
+end
+
+function App:on_activate()
+  self.active_window:set_titlebar(Gtk.HeaderBar({
+    visible = true,
+    show_close_button = true,
+    title = "GtkLabel",
+    subtitle = "Example 1"
+  }))
+
+  local Label = Gtk.Label({
+    visible = true,
+    valign = Gtk.Align.CENTER,
+    halign = Gtk.Align.CENTER,
+    -- The use_markup property to use an HTML-like
+    -- basic markup:
+    use_markup = true,
+    label = "<span size='xx-large' font_weight='bold'>Hello, world!</span>"
+  })
+
+  self.active_window:add(Label)
+  self.active_window:present()
+end
+
+return App:run(arg)
