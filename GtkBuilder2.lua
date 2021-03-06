bc1fb6ef483d08356c6afe8a025c557b2bb293b1
Miqueas Martinez <miqueas2020@yahoo.com>
2021-03-06 01:52:42 -0500

    Added GtkBuilder example 2

---
+++ GtkBuilder2.lua
@@ -0,0 +1,21 @@
+local lgi = require("lgi")
+local Gtk = lgi.require("Gtk", "3.0")
+
+local Builder = Gtk.Builder.new_from_file("GtkBuilder.ui")
+
+-- Another way to use GtkBuilder in LGI
+local UI      = Builder.objects
+
+local App     = Gtk.Application({
+  application_id = "com.github.moonsteal.lua-gtk-examples.GtkBuilder2"
+})
+
+function App:on_startup()
+  self:add_window(UI.Window)
+end
+
+function App:on_activate()
+  self.active_window:present()
+end
+
+return App:run(arg)
