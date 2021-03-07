d8747c55466d729d47441a0acb6602180d61daa2
Miqueas Martinez <miqueas2020@yahoo.com>
2021-03-06 20:50:29 -0500

    Added GtkActionBar example

---
+++ GtkActionBar.lua
@@ -0,0 +1,36 @@
+local lgi = require("lgi")
+local Gtk = lgi.require("Gtk", "3.0")
+
+local App = Gtk.Application({
+  application_id = "com.github.moonsteal.lua-gtk-examples.GtkActionBar"
+})
+
+function App:on_startup()
+  Gtk.ApplicationWindow({
+    application = self,
+    default_width = 400,
+    default_height = 400
+  })
+end
+
+function App:on_activate()
+  self.active_window:set_titlebar(Gtk.HeaderBar({
+    visible = true,
+    show_close_button = true,
+    title = "Your app title",
+    subtitle = "Your app subtitle"
+  }))
+
+  local Box = Gtk.Box({ visible = true, orientation = Gtk.Orientation.VERTICAL })
+  local Bar = Gtk.ActionBar({ visible = true })
+  Bar:pack_start(Gtk.Label({ visible = true, label = "Something" }), false, true, 0)
+  Bar:pack_end(Gtk.Button({ visible = true, label = "A button" }), false, true, 0)
+
+  Box:pack_start(Gtk.Label({ visible = true, label = "Here is the content of your app" }), true, true, 0)
+  Box:pack_end(Bar, false, true, 0)
+
+  self.active_window:add(Box)
+  self.active_window:present()
+end
+
+return App:run(arg)
