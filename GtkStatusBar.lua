a5e46229c3f13b6a3c2e455ffe3c88bf45d71bed
Miqueas Martinez <miqueas2020@yahoo.com>
2021-03-08 02:08:41 -0500

    Added GtkStatusbar example

---
+++ GtkStatusBar.lua
@@ -0,0 +1,52 @@
+local lgi = require("lgi")
+local Gtk = lgi.require("Gtk", "3.0")
+
+local App = Gtk.Application({
+  application_id = "com.github.moonsteal.lua-gtk-examples.GtkStatusbar"
+})
+
+function App:on_startup()
+  Gtk.ApplicationWindow({
+    application = self,
+    default_width = 400,
+    default_height = 200
+  })
+end
+
+function App:on_activate()
+  self.active_window:set_titlebar(Gtk.HeaderBar({
+    visible = true,
+    show_close_button = true,
+    title = "GtkStatusbar"
+  }))
+
+  --[[ GtkActionBar:
+
+    A widget like GtkActionBar for report some info to the user.
+
+  ]]
+  local Status = Gtk.Statusbar({ visible = true })
+
+  local Btn = Gtk.Button({
+    visible = true,
+    label = "Click me",
+    halign = Gtk.Align.CENTER,
+    valign = Gtk.Align.CENTER
+  })
+
+
+  local c = 0
+  function Btn:on_clicked()
+    c = c + 1
+    Status:push(c, "You clicked " .. c .. " times")
+  end
+
+  local Box = Gtk.Box({ visible = true, orientation = Gtk.Orientation.VERTICAL })
+  Box:pack_start(Btn, true, true, 0)
+  Box:pack_end(Status, false, true, 0)
+
+  self.active_window:add(Box)
+  self.active_window:present()
+end
+
+return App:run(arg)
