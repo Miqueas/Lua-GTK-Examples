3f7d9a152dd096130490a86a378eedfb449dc9e6
Miqueas Martinez <miqueas2020@yahoo.com>
2021-03-07 20:34:28 -0500

    Added GtkExpander example

---
+++ GtkExpander.lua
@@ -0,0 +1,38 @@
+local lgi = require("lgi")
+local Gtk = lgi.require("Gtk", "3.0")
+
+local App = Gtk.Application({
+  application_id = "com.github.moonsteal.lua-gtk-examples.GtkExpander"
+})
+
+function App:on_startup()
+  Gtk.ApplicationWindow({
+    application = self,
+    default_width = 400,
+    border_width = 10
+  })
+end
+
+function App:on_activate()
+  self.active_window:set_titlebar(Gtk.HeaderBar({
+    visible = true,
+    show_close_button = true,
+    title = "Your app title",
+    subtitle = "Your app subtitle"
+  }))
+
+  local LoremIpsum = "Duis in metus eros. Duis faucibus rutrum eros eu vestibulum. Proin et arcu nulla. Etiam at lacinia nibh. Vivamus pellentesque nunc nibh, ac dignissim massa lobortis ut. Integer eu felis in elit semper ullamcorper at in ipsum. Suspendisse tempus massa vel nibh tristique vestibulum. Vestibulum varius eu nunc eu interdum. Curabitur pulvinar velit in purus facilisis, et auctor augue consequat. Donec finibus felis ligula, a convallis justo tristique a."
+  local Expand = Gtk.Expander({
+    visible = true,
+    label = [[  <span size="large" font_weight="bold">Lorem ipsum</span>  ]],
+    use_markup = true,
+    resize_toplevel = true,
+
+    Gtk.Label({ visible = true, label = LoremIpsum, wrap = true })
+  })
+
+  self.active_window:add(Expand)
+  self.active_window:present()
+end
+
+return App:run(arg)
