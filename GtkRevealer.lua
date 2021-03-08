3747631d1fead17a91c8daec84d9a691cd34b760
Miqueas Martinez <miqueas2020@yahoo.com>
2021-03-07 20:17:57 -0500

    Fixed id

--- GtkRevealer.lua
+++ GtkRevealer.lua
@@ -2,7 +2,7 @@ local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "3.0")
 
 local App = Gtk.Application({
-  application_id = "com.github.moonsteal.lua-gtk-examples.GtkPaned"
+  application_id = "com.github.moonsteal.lua-gtk-examples.GtkRevealer"
 })
 
 function App:on_startup()
