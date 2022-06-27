911a9040582e71afffc2527d7be809d27fbe7630
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-06-27 08:10:08 +0200

    Lua's alternative syntax used

--- GtkButton2.lua
+++ GtkButton2.lua
@@ -4,34 +4,34 @@ if arg[#arg] == "-debug" then require"mobdebug".start() end
 local lgi = require"lgi"
 local Gtk = lgi.require("Gtk", "3.0")
 
-local App = Gtk.Application({
+local App = Gtk.Application{
   application_id = "com.github.Miqueas.Lua-GTK3-Examples.GtkButton2"
-})
+}
 
 function App:on_startup()
-  Gtk.ApplicationWindow({
+  Gtk.ApplicationWindow{
     application = self,
     default_width = 400,
     default_height = 400
-  })
+  }
 end
 
 function App:on_activate()
   local window = self.active_window
   
-  window:set_titlebar(Gtk.HeaderBar({
+  window:set_titlebar(Gtk.HeaderBar{
     visible = true,
     show_close_button = true,
     title = "GtkButton",
     subtitle = "Example 2"
-  }))
+  })
 
-  local Button = Gtk.Button({
+  local Button = Gtk.Button{
     visible = true,
     label = "Click me!",
     valign = Gtk.Align.CENTER,
     halign = Gtk.Align.CENTER
-  })
+  }
 
   local Notification_ID = 1
 
