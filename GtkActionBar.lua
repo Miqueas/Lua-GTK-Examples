076e5e246c44a00c79a57c2b429a59043544a9ec
Miqueas <miqueas2020@yahoo.com>
2022-11-08 08:45:49 -0500

    Gonna update all this shit

--- GtkActionBar.lua
+++ GtkActionBar.lua
@@ -1,40 +1,34 @@
 local lgi = require("lgi")
 local Gtk = lgi.require("Gtk", "3.0")
 
-local App = Gtk.Application({
-  application_id = "com.github.Miqueas.Lua-GTK3-Examples.GtkActionBar"
-})
+-- GtkActionBar: A full width container to add contextual actions.
 
-function App:on_startup()
-  Gtk.ApplicationWindow({
+local app_id = "io.github.Miqueas.Lua-GTK-Examples.Gtk3.ActionBar"
+local app_title = "GtkActionBar"
+local app = Gtk.Application { application_id = app_id }
+
+function app:on_startup()
+  local win = Gtk.ApplicationWindow {
+    title = app_title,
     application = self,
     default_width = 400,
     default_height = 400
-  })
+  }
+
+  local bar = Gtk.ActionBar({ visible = true })
+  local box = Gtk.Box({ visible = true, orientation = Gtk.Orientation.VERTICAL })
+
+  bar:pack_start(Gtk.Label({ visible = true, label = "Something" }))
+  bar:pack_end(Gtk.Button({ visible = true, label = "A button" }))
+
+  box:pack_start(Gtk.Label({ visible = true, label = "Here is the content of your app" }), true, true, 0)
+  box:pack_end(bar, false, true, 0)
+
+  win:add(box)
 end
 
-function App:on_activate()
-  self.active_window:set_titlebar(Gtk.HeaderBar({
-    visible = true,
-    show_close_button = true,
-    title = "GtkActionBar"
-  }))
-
-  --[[ GtkActionBar:
-
-    A full width container to add contextual actions.
-
-  ]]
-  local Bar = Gtk.ActionBar({ visible = true })
-  Bar:pack_start(Gtk.Label({ visible = true, label = "Something" }))
-  Bar:pack_end(Gtk.Button({ visible = true, label = "A button" }))
-
-  local Box = Gtk.Box({ visible = true, orientation = Gtk.Orientation.VERTICAL })
-  Box:pack_start(Gtk.Label({ visible = true, label = "Here is the content of your app" }), true, true, 0)
-  Box:pack_end(Bar, false, true, 0)
-
-  self.active_window:add(Box)
+function app:on_activate()
   self.active_window:present()
 end
 
-return App:run(arg)
+return app:run(arg)
